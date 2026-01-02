b2e3fe20408228480b206be0f4e82305282e4142
abhishekmohale11 <abhishek.mohale11@gmail.com>
2026-01-02 18:01:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -29,7 +29,7 @@ document.addEventListener("mousemove",(e)=>{
 window.addEventListener("load", () => {
   const colors = glitterColors();
   confetti({
-    particleCount: 200,
+    particleCount: 4,
     spread: 180,
     origin: { x: 0.5, y: 0 },
     colors: colors,
@@ -116,4 +116,5 @@ function goNext(){
     window.addEventListener("click", (e) => { if(e.target === modal) modal.style.display="none"; });
   }
 }
+
 
